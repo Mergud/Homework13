3968e791fb112407420bb4c2f1ac067332338ad8
Mergud <34305136+Mergud@users.noreply.github.com>
2017-12-12 17:08:55 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+/*
+var fs = require ("fs");
+
+fs.appendFile('h.txt', '\nSecond sentence', function (err){
+	if(err) throw err;
+	console.log('Updated!');
+});
+*/
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: 'PanAndre186@gmail.com',
+		pass: '10199715pp'
+	}
+});
+
+var mailOptions = {
+	from: 'PanAndre186@gmail.com',
+	to: 'Zmilan66@gmail.com',
+	subject: 'Sending Email using Node.js',
+	text: 'That was strange'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+	if (error) {
+    console.log(error);
+  	} else {
+    console.log('Email sent succesfully: ' + info.response);
+  }
+});
